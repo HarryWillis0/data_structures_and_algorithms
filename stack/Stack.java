6016f2bec1b36397dc07e8938af2dab767274505
Harry Willis <hgwillis@uvic.ca>
2019-05-29 09:26:49 -0600

    stack interface / template
    template
    
    finish template
    
    template and interface done

--- stack/Stack.java
+++ stack/Stack.java
@@ -14,29 +14,42 @@ interface Stack<T>{
      */
     public boolean isEmpty();
 
-    // todo
     /**
      * peek()
-     * 
+     * @return item at top of stack, without changing the stack
+     * @throws exception if stack empty 
      */
     public T peek() throws Exception;
 
     /**
      * pop()
-     * 
+     * Remove item at top of stack
+     * @return item at top of stack
+     * @throws exception if stack empty
      */
     public T pop() throws Exception;
 
     /**
      * push(T item)
-     * 
+     * Add item to top of stack
+     * @return the item
      */
     public T push(T item);
+    
     /**
      * search(T item)
-     *  
+     * Search for item in stack
+     * @return index of item in stack
+     * @throws exception if stack is empty
      */
-    public int search(T item);
+    public int search(T item) throws Exception;
+
+    /**
+     * toString()
+     * Put stack in string format
+     * @return stack in string
+     */
+    public String toString();
 
 }
 
