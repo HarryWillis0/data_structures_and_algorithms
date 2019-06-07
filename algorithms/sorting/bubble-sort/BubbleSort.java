55c1ed1b29eb1fbbbf1444c4d70b6f48c8e6465f
Harry <harry.willis0@gmail.com>
2019-06-07 12:36:38 -0600

    implemented & tested

--- algorithms/sorting/bubble-sort/BubbleSort.java
+++ algorithms/sorting/bubble-sort/BubbleSort.java
@@ -1,10 +1,26 @@
 /**
- * A bubble sort implementation in java
+ * A bubble sort implementation for int arrays in java
  * 
  * @author Harry Willis
  */
  public class BubbleSort {
-    public void bubbleSort(int[] a) {
-        
+    public static void bubbleSort(int[] a) {
+        // sorted flag
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    flag = true;
+
+                    //swap
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                }
+            }
+        }
     }
 }
