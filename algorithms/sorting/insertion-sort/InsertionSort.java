bf369e62a1ce5d8189b2b2548e0f499bb82b7075
Harry <harry.willis0@gmail.com>
2019-06-07 13:17:06 -0600

    implemented insertion sort and tested

--- algorithms/sorting/insertion-sort/InsertionSort.java
+++ algorithms/sorting/insertion-sort/InsertionSort.java
@@ -5,6 +5,18 @@
  */
 public class InsertionSort {
     public static void insertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int curr = a[i];
+            
+            int j = i - 1;
 
+            while (j >= 0 && a[j] > curr) {
+                // shift item
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            a[j + 1] = curr;
+        }
     }
 }
