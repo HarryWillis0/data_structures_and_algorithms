069d1bf09a4e29e8f0d2647a5cab89c2e8675002
Harry <harry.willis0@gmail.com>
2019-06-07 15:28:27 -0600

    add function description, rename flag, rename parameter

--- algorithms/sorting/bubble-sort/BubbleSort.java
+++ algorithms/sorting/bubble-sort/BubbleSort.java
@@ -4,21 +4,29 @@
  * @author Harry Willis
  */
  public class BubbleSort {
-    public static void bubbleSort(int[] a) {
+
+     /**
+      * bubbleSort(int[] array)
+      * Sorts an array of ints by repeatedly comparing adjacent pairs
+      * If they are out of order they are swapped
+      * bubble sort continues until no swaps are made
+      * @param array
+      */
+    public static void bubbleSort(int[] array) {
         // sorted flag
-        boolean flag = true;
+        boolean swap = true;
 
-        while (flag) {
-            flag = false;
+        while (swap) {
+            swap = false;
 
-            for (int i = 0; i < a.length - 1; i++) {
-                if (a[i] > a[i + 1]) {
-                    flag = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap = true;
 
                     //swap
-                    int temp = a[i];
-                    a[i] = a[i + 1];
-                    a[i + 1] = temp;
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
                 }
             }
         }
