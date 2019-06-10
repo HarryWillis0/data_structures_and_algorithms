6f2808fda2f535f6fa50ad6b6c27f7634dc23e50
Harry <harry.willis0@gmail.com>
2019-06-09 22:16:19 -0600

    conflict resolution

--- data-structures/queue/ArrayQueueTest.java
+++ data-structures/queue/ArrayQueueTest.java
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
-=======
 /**
  * Test program for ArrayQueue.java
  * 
  * @author Harry Willis
  */
->>>>>>> master
 public class ArrayQueueTest {
     public static void main(String[] args){
         ArrayQueue<Integer> intQueue = new ArrayQueue<Integer>();
         try {
-<<<<<<< HEAD
-            System.out.println(intQueue.toString());
-
-            System.out.println("Enqueuing \"1\", \"-40\", \"666\" \"0\", \"999\", and \"123456\"");
-            
-=======
             // toString test
             System.out.println(intQueue.toString());
 
             System.out.println("Enqueuing \"1\", \"-40\", \"666\" \"0\", \"999\", and \"123456\"");
             // enqueue tests
->>>>>>> master
             intQueue.enqueue(1);
             intQueue.enqueue(-40);
             intQueue.enqueue(666);
@@ -31,32 +21,14 @@ public class ArrayQueueTest {
 
             System.out.println("Queue now:\n\t" + intQueue.toString());
 
-<<<<<<< HEAD
-            System.out.println("Dequeuing:\n\t" + intQueue.dequeue());
-
-=======
             // dequeue test
             System.out.println("Dequeuing:\n\t" + intQueue.dequeue());
 
             // poll test
->>>>>>> master
             System.out.println("Polling:\n\t" + intQueue.poll());
 
             System.out.println("Queue now:\n\t" + intQueue.toString());
 
-<<<<<<< HEAD
-            System.out.println("Dequeuing 5 elements:\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue());
-
-            System.out.println("Is queue empty?\n\t" + intQueue.isEmpty());
-
-            System.out.println("Queue now:\n\t" + intQueue.toString());
-        
-        
-        } catch (Exception e) {
-            System.out.println("Queue empty");
-        }
-
-=======
             // more dequeuing
             System.out.println("Dequeuing 5 elements:\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue() + "\n\t" + intQueue.dequeue());
 
@@ -68,6 +40,5 @@ public class ArrayQueueTest {
         } catch (Exception e) {
             System.out.println("Queue empty");
         }
->>>>>>> master
     }
 }
--- data-structures/stack/ArrayStackTest.java
+++ data-structures/stack/ArrayStackTest.java
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
-
-=======
 /**
  * Test program for ArrayStack.java
  * 
  * @author Harry Willis
  */
->>>>>>> master
 public class ArrayStackTest{
     public static void main(String[] args){
         ArrayStack<String> stringStack = new ArrayStack<String>();
 
         try {
-<<<<<<< HEAD
-            System.out.println("Stack:\n\t " + stringStack.toString() + "\n");
-            System.out.println("Pushing \"hello\", \"this\", \"is\", \"a\", \"stack\", \"that\", \"has\", \"to\", and \"extend\"");
-=======
             // toString
             System.out.println("Stack:\n\t " + stringStack.toString() + "\n");
             
             System.out.println("Pushing \"hello\", \"this\", \"is\", \"a\", \"stack\", \"that\", \"has\", \"to\", and \"extend\"");
             // push tests
->>>>>>> master
             stringStack.push("hello");
             stringStack.push("this");
             stringStack.push("is");
@@ -34,14 +25,6 @@ public class ArrayStackTest{
             
             System.out.println("\nStack:\n\t " + stringStack.toString() + "\n");
 
-<<<<<<< HEAD
-            System.out.println("Searching for \"hello\":");
-            System.out.println("\t" + stringStack.search("hello"));
-
-            System.out.println("\nSearching for \"doesn't occur\":");
-            System.out.println("\t" + stringStack.search("doesn't occur"));
-            
-=======
             // search occurs test
             System.out.println("Searching for \"hello\":");
             System.out.println("\t" + stringStack.search("hello"));
@@ -51,26 +34,17 @@ public class ArrayStackTest{
             System.out.println("\t" + stringStack.search("doesn't occur"));
             
             // pop tests
->>>>>>> master
             System.out.println("\nPopping 3 elements:");
             System.out.println("\t" + stringStack.pop()+"\n\t" + stringStack.pop() + "\n\t" + stringStack.pop());
 
             System.out.println("\nStack:\n\t " + stringStack.toString());
 
-<<<<<<< HEAD
-            System.out.println("\nStack empty? \n\t" + stringStack.isEmpty());
-
-            System.out.println("\nPeeking at: \n\t" + stringStack.peek());
-
-
-=======
             // isEmpty test
             System.out.println("\nStack empty? \n\t" + stringStack.isEmpty());
 
             // peeking test
             System.out.println("\nPeeking at: \n\t" + stringStack.peek());
 
->>>>>>> master
         } catch (Exception e) {
             System.out.println("Stack is empty");
         }
