97876f4c924acff89ed7f5d494ff0ef9a59c39e6
Harry <harry.willis0@gmail.com>
2019-06-11 12:18:04 -0600

    fix spacing

--- algorithms/sorting/insertion-sort/InsertionSortTester.java
+++ algorithms/sorting/insertion-sort/InsertionSortTester.java
@@ -1,8 +1,5 @@
 import java.util.Arrays;
-<<<<<<< HEAD
-=======
 
->>>>>>> insertion-sort-tester-prog
 /**
  * Tester program for InsertionSort.java
  * 
@@ -22,10 +19,6 @@ public class InsertionSortTester {
         InsertionSort.insertionSort(intArray2);
         System.out.println("Sorted: " + Arrays.toString(intArray2) + "\n");
 
-<<<<<<< HEAD
-
-=======
->>>>>>> insertion-sort-tester-prog
         //already sorted
         int[] intArray3 = {1, 3, 55, 9999, 1001010};
         System.out.println("Insertion sorting: " + Arrays.toString(intArray3));
